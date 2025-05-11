6c79c3372008acffc31889bce6f70f8bc09da26e
Gemunu Abeysundara <86831864+ShanGemunu@users.noreply.github.com>
2025-05-11 14:38:34 +0530

    change user factory and database seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => 'admin@mail.com',
             'user_type' => 'admin',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('1234'),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(3)->create();
+        User::factory(1)->create();
 
         PoyaDay::factory(12)->create();
     }
